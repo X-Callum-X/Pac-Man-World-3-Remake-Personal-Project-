d27f1d6b7c1eebd2222044f3c52b98cf3c887c61
Callum <ckelly1536@outlook.com>
2026-04-14 17:28:35 +0100

    Dialogue System
    Added a dialogue system where the player walks into a trigger, and there is an animation that brings the dialogue box with text up and down at the start and end of dialogue respectively
    Dialogue system works with voicelines
    Voicelines are stopped if dialogue is skipped so that they don't overlap
    Added Orson's model to the project with an idle animation

---
+++ Assets/Programming/UI/DialogueManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+
+    public Image characterIcon;
+    public TextMeshProUGUI characterName;
+    public TextMeshProUGUI dialogueArea;
+
+    private Queue<DialogueLine> lines;
+
+    public bool isDialogueActive = false;
+
+    public float typingSpeed = 0.2f;
+
+    public Animator animator;
+
+    public AudioSource source;
+    private AudioClip voiceLine;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        lines = new Queue<DialogueLine>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            source.Stop();
+
+            DisplayNextDialogueLine();
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        isDialogueActive = true;
+
+        animator.Play("show");
+
+        lines.Clear();
+
+        foreach (DialogueLine dialogueLine in dialogue.dialogueLines)
+        {
+            lines.Enqueue(dialogueLine);
+        }
+
+        DisplayNextDialogueLine();
+    }
+
+    public void DisplayNextDialogueLine()
+    {
+        if (lines.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        DialogueLine currentLine = lines.Dequeue();
+
+        characterIcon.sprite = currentLine.character.icon;
+        characterName.text = currentLine.character.name;
+        voiceLine = currentLine.character.clip;
+
+        source.PlayOneShot(voiceLine);
+
+        StopAllCoroutines();
+
+        StartCoroutine(TypeSentence(currentLine));
+    }
+
+    IEnumerator TypeSentence(DialogueLine dialogueLine)
+    {
+        dialogueArea.text = "";
+        foreach (char letter in dialogueLine.line.ToCharArray())
+        {
+            dialogueArea.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    void EndDialogue()
+    {
+        isDialogueActive = false;
+        animator.Play("hide");
+    }
+}
